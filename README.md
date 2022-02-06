0249f708295a1e5a4b2d406bfb31ea37309ee392
Helbert <helbert.aguiar@hotmail.com>
2022-02-05 21:16:24 -0300

    update design of project and starting new development for solver captcha

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# consulta_AR
-efetua consulta de status no correios
+# Consulta Status de AR dos Correios
+
+The Brazilian Post Office and Telegraph Corporation, also known as the "Correios", is a state-owned company that operates Brazil's national postal service.
+
+Towards using an API of their corporation that return the situation of "package send" through of an code vinculate also known AR ( Aviso de Recebimento ) use this code in pyth
+
+## Features
+
+(..)
+
+<span style="color: red; text-transform:uppercase; font-weight: bold;"> Attention please: </span> [Portuguese] Api foi atualizada e é necessário resolver um CAPTCHA agora. Está sendo feito um estudo de viabilidade e, se possível será implementado.
+
+## Usage
+
+1.  Install dependencies on requirements.txt
+1.  Execute the code on terminal or cmd: "python main.py"
+
+## Links
+
+* Manuel query status AR: https://rastreamento.correios.com.br/app/index.php
--- main.py
+++ main.py
@@ -1,51 +1,53 @@
 import requests
 from bs4 import BeautifulSoup
 
-def printRequest(r, method, url_solicitada, form=None):
-    print('\nURL Solicitada da requisicao: '.ljust(28, ' ') + url_solicitada)
-    print('URL Efetiva da requisicao: '.ljust(30, ' ') + r.url)
-    print('History redirect: '.ljust(30, ' ') + str(r.history))
-    print('Status code: '.ljust(30, ' ') +
-          str(r.status_code) + '\tReason: ' + str(r.reason))
-    print('Elapsed: '.ljust(30, ' ') + str(r.elapsed) +
-          '\tEnconding: ' + str(r.encoding))
-    if form != None:
-        printForm(form)
-    print('\nOs headers da requisicao do ' + method + ' sao: ' + '\n')
-    print("\n".join("{} {}".format(k.ljust(28, ' '), v.ljust(30, ' '))
-                    for k, v in r.request.headers.items()))
-    print('\nOs headers da resposta do ' + method + ' sao: ' + '\n')
-    print("\n".join("{} {}".format(k.ljust(28, ' '), v.ljust(30, ' '))
-                    for k, v in r.headers.items()))
-    print('\n' + ''.ljust(158, '='))
+# TO:DO Implements an Solver Captcha 
+
+class RequestApi():
+
+    url_custom_api:str = 'https://www2.correios.com.br/sistemas/rastreamento/ctrl/ctrlRastreamento.cfm'
+
+    def request(self, code_to_tracker):
+        with requests.Session() as session:
+            session.headers.update({'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:88.0) Gecko/20100101 Firefox/88.0'})
+            form = {'acao': 'track',
+                    'objetos': code_to_tracker,
+                    'btnPesq': 'Buscar',
+                    }
+            return session.post(self.url_custom_api, data=form, allow_redirects=True)
+    
+    # def printRequest(self, r, method, url_solicitada, form=None):
+    #     print('\nURL Solicitada da requisicao: '.ljust(28, ' ') + url_solicitada)
+    #     print('URL Efetiva da requisicao: '.ljust(30, ' ') + r.url)
+    #     print('History redirect: '.ljust(30, ' ') + str(r.history))
+    #     print('Status code: '.ljust(30, ' ') +
+    #         str(r.status_code) + '\tReason: ' + str(r.reason))
+    #     print('Elapsed: '.ljust(30, ' ') + str(r.elapsed) +
+    #         '\tEnconding: ' + str(r.encoding))
+    #     if form != None:
+    #         printForm(form)
+    #     print('\nOs headers da requisicao do ' + method + ' sao: ' + '\n')
+    #     print("\n".join("{} {}".format(k.ljust(28, ' '), v.ljust(30, ' '))
+    #                     for k, v in r.request.headers.items()))
+    #     print('\nOs headers da resposta do ' + method + ' sao: ' + '\n')
+    #     print("\n".join("{} {}".format(k.ljust(28, ' '), v.ljust(30, ' '))
+    #                     for k, v in r.headers.items()))
+    #     print('\n' + ''.ljust(158, '='))
 
 
-def printForm(form):
-    print('\nForm da requisicao:')
-    print("\n".join("{} ".format(k) for k in form.items()))
+    # def printForm(self, form):
+    #     print('\nForm da requisicao:')
+    #     print("\n".join("{} ".format(k) for k in form.items()))
 
-with requests.Session() as session:
+class TrackerAR():
 
-    session.headers.update({'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:88.0) Gecko/20100101 Firefox/88.0'})
+    def query(self, code_ar):
+        req = RequestApi()
+        response = req.request(code_ar)
+        soup = BeautifulSoup(response.content, "html.parser")
 
-    url_custom = 'https://www2.correios.com.br/sistemas/rastreamento/ctrl/ctrlRastreamento.cfm'
-    ar = 'BJ230414885BR'
-    # ar = 'BK285863866BR'
+        with open("log.html", "w") as f:
+            f.write(response.text)
 
-    form = {'acao': 'track',
-            'objetos': ar,
-            'btnPesq': 'Buscar',
-            }
-
-    r = session.post(url_custom, data=form, allow_redirects=True)
-    soup = BeautifulSoup(r.content, "html.parser")
-
-    status = "Não entregue"
-    tags = soup.find_all('strong')    
-    for tag in tags:
-        if tag.text == 'Objeto entregue ao destinatário':
-            status = "Entregue"
-
-
-    print(status)
-    # print(soup.encode("utf-8"))
+tracker = TrackerAR()
+tracker.query("BK285877825BR")
